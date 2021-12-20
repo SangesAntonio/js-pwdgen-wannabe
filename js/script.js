c068a5ea9c750f3ef1b2e96e96427425f8464c91
SangesAntonio <sanges65@gmail.com>
2021-12-20 14:57:22 +0100

    added prompt

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
 console.log("js ok")
 
+//generatore passworld
+
+/*Inserire nome
+    1: Inserire nome
+    2:inserire cognome
+    3:Inserire colore preferito*/
+
+    //inserito prompt per il nome
+    const myName = prompt("Il tuo nome?")
+    console.log(myName)
+
+    //inserito prompt per il cognome
+    const mySecondName = prompt("Il tuo cognome?")
+    console.log(mySecondName)
+
+    //inserito prompt per il colore
+    const myColor = prompt("Il tuo colore preferito?")
+    console.log(myColor)
+
+
